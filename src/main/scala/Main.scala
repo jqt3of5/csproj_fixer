5526a752e093b7de847ef7e4fe2b9081d3d6457d
jqtodd@xactware.com <jqtodd@xactware.com>
2021-02-02 11:23:01 -0700

    It seems to work! Time for a test run.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,7 @@
 import java.io.{File, FileFilter, FilenameFilter}
-
-import scala.xml.XML
-
+import java.nio.file.Path
+import scala.xml.{Elem, Node, NodeSeq, UnprefixedAttribute, XML}
+import scala.xml.transform.{RewriteRule, RuleTransformer}
 
 object IsFile extends FileFilter {
   override def accept(pathname: File): Boolean = pathname.isFile
@@ -14,6 +14,7 @@ object IsCSProj extends FilenameFilter {
 }
 
 object Main extends App {
+
   def debug[A](input : A) : A = {
     println(input)
     input
@@ -25,17 +26,62 @@ object Main extends App {
     dir.listFiles(filter).concat(files).toList
   }
 
+  case class CSProj(file : File) {
+    val xml: Elem = XML.loadFile(file)
+    val name: String = file.getName.substring(0, file.getName.lastIndexOf('.'))
+    val path : Path = file.toPath
+    val guid: String =(xml \ "PropertyGroup" \ "ProjectGuid").text
+    val dependencyNames: Seq[String] = (xml \ "ItemGroup" \ "Reference" ).map(ref => (ref \@ "Include").takeWhile(c => c != ','))
+    def relativeTo(other : String): String = Path.of(other).relativize(path).toString
+  }
+
   //Load csproj files
-  def foo = enumerateAllFiles(new File("D:\\sd\\ConvertDllReference"), IsCSProj)
-    //Parse xml files
-    .map(file => (file.getAbsolutePath, file.getName,XML.loadFile(file)))
-    //Locate dll references
-    //Gets file path (For linking), project guid, And reference tags (To remove them?)
-    .map({
-      case (path, name, xml) => (path, name, (xml \ "PropertyGroup" \ "ProjectGuid").text, (xml \ "ItemGroup" \ "Reference" ))
+  def getProjInfo= enumerateAllFiles(new File("ConvertDllReference"), IsCSProj)
+    //Parse xml files, get path, and proj name (Minus extension)
+    .map(file => new CSProj(file))
+
+  //Resolve the project guid/relative path for each dependency listed
+  def resolveProjRefs = getProjInfo.map { proj =>
+    //Gets the csProj object for each named dependency, if it's ours (exclude system/nuget packages)
+    val ourDependencies = getProjInfo.filter(p => proj.dependencyNames.contains(p.name))
+    val ourDependencyNames = ourDependencies.map(p => p.name)
+
+    val projRefXml =
+      <ItemGroup>
+        { ourDependencies.map { dep =>
+            <ProjectReference Include={dep.relativeTo(proj.path.toString)}>
+              <Project>{dep.guid}</Project>
+              <Name>{dep.name}</Name>
+            </ProjectReference>
+          }
+        }
+      </ItemGroup>
+
+    val newXml = new RuleTransformer(Main.addProjRefRule(projRefXml), Main.removeDllRefRule(ourDependencyNames)).transform(proj.xml)
+    newXml.foldRight(0)((x,n) => {
+      XML.save(proj.name+n+".xml", x)
+      n+1
     })
 
-  //Correlate to csproj (in list)
+    println(newXml)
+   //Returns the csProj, the resolved csproj dependencies, and the new project reference xml
+    (proj, newXml)
+  }
+
+  def removeDllRefRule(dependencyNames : List[String]) = new RewriteRule {
+    override def transform(n: Node): collection.Seq[Node] = n match {
+      case elem : Elem if elem.label == "Reference" && dependencyNames.foldRight(false){ (name, acc) => acc || name.startsWith(elem \@ "Include") } => NodeSeq.Empty
+      case n => n
+    }
+  }
+
+  def addProjRefRule(projRefXML : Node) = new RewriteRule {
+    override def transform(n: Node): collection.Seq[Node] = n match {
+      case elem : Elem if elem.label == "Project" => elem.copy(child =  projRefXML ++ elem.child)
+      case n => n
+    }
+  }
+
   //Delete dll reference
   //Insert csproj reference
 }
