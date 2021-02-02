38e891bcc136fc1020fecc145d44cd7f1a36931a
jqtodd@xactware.com <jqtodd@xactware.com>
2021-02-02 16:53:45 -0700

    Passed phase one of testing. Now for a trial run on real data.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,6 @@
-import java.io.{File, FileFilter, FilenameFilter}
+import java.io.{BufferedWriter, File, FileFilter, FileWriter, FilenameFilter}
 import java.nio.file.Path
-import scala.xml.{Elem, Node, NodeSeq, UnprefixedAttribute, XML}
+import scala.xml.{Elem, Node, NodeSeq, PrettyPrinter, UnprefixedAttribute, XML}
 import scala.xml.transform.{RewriteRule, RuleTransformer}
 
 object IsFile extends FileFilter {
@@ -29,10 +29,10 @@ object Main extends App {
   case class CSProj(file : File) {
     val xml: Elem = XML.loadFile(file)
     val name: String = file.getName.substring(0, file.getName.lastIndexOf('.'))
-    val path : Path = file.toPath
+    val path : Path = file.toPath.toAbsolutePath
     val guid: String =(xml \ "PropertyGroup" \ "ProjectGuid").text
     val dependencyNames: Seq[String] = (xml \ "ItemGroup" \ "Reference" ).map(ref => (ref \@ "Include").takeWhile(c => c != ','))
-    def relativeTo(other : String): String = Path.of(other).relativize(path).toString
+    def relativeTo(other : Path): Path = debug(other.getParent).relativize(debug(path))
   }
 
   //Load csproj files
@@ -49,35 +49,37 @@ object Main extends App {
     val projRefXml =
       <ItemGroup>
         { ourDependencies.map { dep =>
-            <ProjectReference Include={dep.relativeTo(proj.path.toString)}>
+            <ProjectReference Include={dep.relativeTo(proj.path).toString}>
               <Project>{dep.guid}</Project>
               <Name>{dep.name}</Name>
-            </ProjectReference>
-          }
-        }
+            </ProjectReference>}}
       </ItemGroup>
 
+    val pp = new PrettyPrinter(800, 4)
     val newXml = new RuleTransformer(Main.addProjRefRule(projRefXml), Main.removeDllRefRule(ourDependencyNames)).transform(proj.xml)
+    val writer = new BufferedWriter(new FileWriter(proj.path.toString, false))
     newXml.foldRight(0)((x,n) => {
-      XML.save(proj.name+n+".xml", x)
+        println(pp.format(x))
+        writer.write(pp.format(x))
       n+1
     })
+    writer.close()
 
-    println(newXml)
    //Returns the csProj, the resolved csproj dependencies, and the new project reference xml
     (proj, newXml)
   }
 
   def removeDllRefRule(dependencyNames : List[String]) = new RewriteRule {
+    override def transform(ns: collection.Seq[Node]): collection.Seq[Node] = super.transform(ns)
     override def transform(n: Node): collection.Seq[Node] = n match {
-      case elem : Elem if elem.label == "Reference" && dependencyNames.foldRight(false){ (name, acc) => acc || name.startsWith(elem \@ "Include") } => NodeSeq.Empty
+      case elem : Elem if elem.label == "Reference" && dependencyNames.foldRight(false){ (name, acc) => acc || (elem \@ "Include").startsWith(name) } => NodeSeq.Empty
       case n => n
     }
   }
 
   def addProjRefRule(projRefXML : Node) = new RewriteRule {
     override def transform(n: Node): collection.Seq[Node] = n match {
-      case elem : Elem if elem.label == "Project" => elem.copy(child =  projRefXML ++ elem.child)
+      case elem : Elem if elem.label == "Project" && !elem.text.matches("[a-z0-9-{}]*")=> elem.copy(child = elem.child ++ projRefXML)
       case n => n
     }
   }
